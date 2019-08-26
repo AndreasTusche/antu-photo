4b770e6c8785ccb800ddc7ff99ae640da2bcbc21
Andreas Tusche <git@andreas-tusche.de>
2019-08-26 17:05:19 +0200

    sanity check for having loaded the config and the library

--- photo-sort-time-frame.bash
+++ photo-sort-time-frame.bash
@@ -34,14 +34,26 @@
 # ---------- ---- --------------------------------------------------------------
 # 2017-04-15 AnTu created
 # 2018-12-30 AnTu now checking for SequenceNumber
+# 2019-08-25 AnTu corrected syntax for handling of SequenceNumber
 
-
+DEBUG=1
 
 # --- nothing beyond this line needs configuration -----------------------------
-if [ "$ANTU_PHOTO_CFG_DONE" != "1" ] ; then
+if [ "$ANTU_PHOTO_CFG_DONE" != "1" ] ; then # read the configuration file(s)
 	for d in "${0%/*}" ~ . ; do source "$d/.antu-photo.cfg" 2>/dev/null || source "$d/antu-photo.cfg" 2>/dev/null; done
 fi
+if [ "$ANTU_PHOTO_CFG_DONE" != "1" ] ; then # if sanity check failed
+	echo -e "\033[01;31mERROR:\033[00;31m Config File antu-photo.cfg was not found\033[0m" >&2 
+	exit 1
+fi
+
 (($PHOTO_LIB_DONE)) || source "$LIB_antu_photo"
+if [ "$PHOTO_LIB_DONE" != "1" ] ; then # if sanity check failed
+	echo -e "\033[01;31mERROR:\033[00;31m Library $LIB_antu_photo was not found\033[0m" >&2
+	exit 1
+fi
+
+
 
 INDIR="$(  readlink -f "${1:-$(pwd)}" )"
 OUTDIR="$( readlink -f "${2:-${DIR_PIC}}" )"
@@ -53,25 +65,26 @@ printToLog "... OUTDIR = $OUTDIR"
 # @ToDo: Sidecar files .cos .dop .nks .pp3 .?s.spd .xmp 
 printInfo  "... sorting by time and sequence number (if any)"
 exiftool -ext "*" --ext DS_Store --ext localized -i SYMLINKS \
-	-if2 '$SequenceNumber' -if2 '$SequenceNumber ne Single'-m -r -progress: -q ${DEBUG:+"-v"} \
+	-if '$SequenceNumber' -if '$SequenceNumber ne "Single"' -if '$SequenceNumber ne 0' \
+	-m -r -progress: -q ${DEBUG:+"-v"} \
 	-d "${OUTDIR%/}/%Y/%Y-%m-%d/%Y%m%d-%H%M%S"\
 	'-FileName<${FileModifyDate}_${SequenceNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
 	'-FileName<${ModifyDate}_${SequenceNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
 	'-FileName<${DateTimeOriginal}_${SequenceNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
 	'-FileName<${CreateDate}_${SequenceNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
-	"${INDIR}" | tee -a ${LOGFILE}
+	"${INDIR}" | tee -a ${LOGFILE} | grep -v "(failed condition)"
 	
 printInfo "... sorting by time and frame (if any)"
 exiftool -ext "*" --ext DS_Store --ext localized -i SYMLINKS \
-    -if2 '$FrameNumber' -m -r -progress: -q  ${DEBUG:+"-v"} \
+    -if '$FrameNumber' -m -r -progress: -q  ${DEBUG:+"-v"} \
     -d "${OUTDIR%/}/%Y/%Y-%m-%d/%Y%m%d-%H%M%S"\
     '-FileName<${FileModifyDate}_${FrameNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
     '-FileName<${ModifyDate}_${FrameNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
     '-FileName<${DateTimeOriginal}_${FrameNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
     '-FileName<${CreateDate}_${FrameNumber;s/\b(\d)\b/0$1/g}%+2c.${FileTypeExtension}'\
-    "${INDIR}" | tee -a ${LOGFILE}
+    "${INDIR}" | tee -a ${LOGFILE} | grep -v "(failed condition)"
 
-printInfo "... sorting remaining by time stamps"
+printInfo "... sorting by time stamps"
 exiftool -ext "*" --ext DS_Store --ext localized -i SYMLINKS \
     -if2 '$CreateDate || $DateTimeOriginal || $ModifyDate' -m -r -progress: -q  ${DEBUG:+"-v"} \
     -d "${OUTDIR%/}/%Y/%Y-%m-%d/%Y%m%d-%H%M%S"\
@@ -79,12 +92,12 @@ exiftool -ext "*" --ext DS_Store --ext localized -i SYMLINKS \
     '-FileName<${DateTimeOriginal}%+2c.${FileTypeExtension}'\
     '-FileName<${CreateDate}%+2c.${FileTypeExtension}'\
     "${INDIR}" | tee -a ${LOGFILE}
+
 # not ideal but the last resort to get a timestamp
 # (having this in above block did not work for some reason)
+printInfo "... sorting remaining by file times"
 exiftool -ext "*" --ext DS_Store --ext localized -i SYMLINKS \
     -fast -m -r -progress: -q  ${DEBUG:+"-v"} \
     -d "${OUTDIR%/}/%Y/%Y-%m-%d/%Y%m%d-%H%M%S"\
     '-FileName<${FileModifyDate}%+2c.${FileTypeExtension}'\
     "${INDIR}" | tee -a ${LOGFILE}
-
-unset DEBUG
